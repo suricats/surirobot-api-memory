18ad202ab7c93b1babe9f5a0a7efb451077ae40a
Alain BERRIER <alain.berrier@outlook.com>
2018-06-26 10:52:14 +0200

    removed pagination

--- api/api/settings.py
+++ api/api/settings.py
@@ -71,8 +71,6 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 15,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
