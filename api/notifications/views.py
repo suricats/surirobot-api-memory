5a77b3ff620d8a733d551d65613b6c36d111acb8
Alain BERRIER <alain.berrier@outlook.com>
2018-07-17 17:56:33 +0200

    more rules

--- api/notifications/views.py
+++ api/notifications/views.py
@@ -27,17 +27,31 @@ class NotificationViewSet(viewsets.ModelViewSet):
     def get_notifications(self, request):
         # Rules engine
         notifications = []
-        sensor = SensorData.objects.filter(type="temperature").latest('created')
+        latest_temp_obj = SensorData.objects.filter(type="temperature").latest('created')
+        latest_humidity_obj = SensorData.objects.filter(type="temperature").latest('humidity')
         # Memorized information
-        latest_temperature = float(sensor.data)
-        date = sensor.created
+        latest_temperature = float(latest_temp_obj.data)
+        latest_humidity = float(latest_humidity_obj.data)
+        date = latest_temp_obj.created
         # Dates
         actual_date = datetime.now(tz=pytz.UTC)
         today = datetime(actual_date.year, actual_date.month, actual_date.day, tzinfo=pytz.UTC)
         # Rule n°1 : temperature is recent and greater than 25°C
-        if date > today and latest_temperature > 25:
+        if date > today and latest_temperature >= 25:
             notifications.append({'type': 'message', 'target': 'all', 'data': 'La temperature est de {}°C. Pensez à bien vous hydrater !'.format(latest_temperature)})
-
+        # Rule n° 2 : temperature is recent and lower than 20°C
+        if date > today and latest_temperature <= 20:
+            notifications.append({'type': 'message', 'target': 'all',
+                                  'data': 'La temperature est de {}°C. Pensez à bien vous couvrir et de boir un café bien chaud !'.format(
+                                      latest_temperature)})
+        # Rule n° 3 : humidity is recent and greater than 80%
+        if date > today and latest_humidity >= 80:
+            notifications.append({'type': 'message', 'target': 'all', 'data': "L'humidité est de {}°C. N'hésitez pas à vous dégourdir les jambes !".format(latest_humidity)})
+        # Rule n° 4 : humidity is recent and lower than 20%
+        if date > today and latest_humidity <= 20:
+            notifications.append({'type': 'message', 'target': 'all',
+                                  'data': "L'humidité est de {}°C. Hydratez vous bien la peau et aérez la pièce.".format(
+                                      latest_humidity)})
         return JsonResponse(notifications, safe=False)
 
     def expiration(self, request):
