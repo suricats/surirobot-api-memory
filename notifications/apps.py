0f4926f955cda71f6bd1ff56f0ad05669dc7ec61
Alain BERRIER <alain.berrier@outlook.com>
2018-08-20 23:00:13 +0200

    add celery for a healthy code

--- notifications/apps.py
+++ notifications/apps.py
@@ -4,10 +4,10 @@ import atexit
 class NotificationsConfig(AppConfig):
     name = 'notifications'
     slack_thread = None
+
     def ready(self):
-        print('oeuf mimosa')
         from .realtime import SlackNotificationsThread
         self.slack_thread = SlackNotificationsThread(interval=10)
         # stop the thread at exit
         atexit.register(self.slack_thread.stop)
-        self.slack_thread.start()
+        # self.slack_thread.start()
